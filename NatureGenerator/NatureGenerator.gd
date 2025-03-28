cfd664ba56a942e4461756ef7ad9f508eacfb5de
Alex K <krab7153@gmail.com>
2025-03-29 00:43:50 +0100

    tree generator

--- NatureGenerator/NatureGenerator.gd
+++ NatureGenerator/NatureGenerator.gd
@@ -4,9 +4,13 @@ extends TileMapLayer
 
 const GRASS_TILE_ID := 0  # grass ID in tileSet
 const SMALL_GRASS_TILE_ID := 1 
+const TREE_TILE_ID := 2
 
+const TREE_MIN_DISTANCE := 2
+const TREE_GRASS_RANGE := 1
 
-@export var grass_density := 0.4
+var grass_density := 0.4
+var tree_density := 0.1
 
 var chunk_renderer
 var noise := FastNoiseLite.new()
@@ -35,18 +39,19 @@ func generate_nature():
 	finalMapWidth = final_map.size()	
 	finalMapHeigth = final_map[0].size()
 	
-	
+	natureMap = []
 	for x in range(finalMapWidth):
 		natureMap.append([])
 		for y in range(finalMapHeigth):
 			natureMap[x].append(-1)
 	
+	_generate_trees()
 	_generate_grass()
 	_generate_small_grass()
 	_renderNatureMap()
 
 func _generate_grass():	
-	clear()
+	
 	if chunk_renderer == null:
 		push_error("nature generator - ChunkRenderer not found")
 		return
@@ -99,9 +104,66 @@ func _generate_small_grass():
 				if randf() < chance:
 					natureMap[x][y] = SMALL_GRASS_TILE_ID
 
+func _generate_trees():
+	noise.frequency = 0.03
+	var noise := FastNoiseLite.new()
+	noise.noise_type = FastNoiseLite.TYPE_SIMPLEX
+	noise.seed = randi()
+
+	for x in range(finalMapWidth):
+		for y in range(finalMapHeigth):
+			var n = noise.get_noise_2d(x, y)
+
+			# tree must be on grass
+			var tile = final_map[x][y]
+			
+			if tile == null:
+				continue
+			if tile.collapsedState != Tiles.getIndex("grass"):
+				continue
+
+			
+			# tree candidate
+			if n > tree_density and natureMap[x][y] == -1:
+				#print('TREE CANDIDAT FOUND AT:', x, ' ', y, '] with noise: ', n)
+				
+				var can_place = true
+				# check nerby trees
+				for dx in range(-TREE_MIN_DISTANCE, TREE_MIN_DISTANCE + 1):
+					for dy in range(-TREE_MIN_DISTANCE, TREE_MIN_DISTANCE + 1):
+						var nx = x + dx
+						var ny = y + dy
+						if nx >= 0 and ny >= 0 and nx < finalMapWidth and ny < finalMapHeigth:
+							if natureMap[nx][ny] == TREE_TILE_ID:
+								#print(' deleted tree cand because tree already exist')
+								can_place = false
+								break
+			
+					if not can_place:
+						break
+				
+				# check nerby grass
+				for dx in range(-TREE_GRASS_RANGE, TREE_GRASS_RANGE + 1):
+					for dy in range(-TREE_GRASS_RANGE, TREE_GRASS_RANGE + 1):
+						var nx = x + dx
+						var ny = y + dy
+						if nx >= 0 and ny >= 0 and nx < finalMapWidth and ny < finalMapHeigth:
+							if final_map[nx][ny].collapsedState != Tiles.getIndex("grass"):
+								can_place = false
+								break
+			
+					if not can_place:
+						break
+				
+				if can_place:
+					natureMap[x][y] = TREE_TILE_ID
 
 func _renderNatureMap():
+	clear()
+		
 	for x in range(finalMapWidth):
 		for y in range(finalMapHeigth):
 			var tileID = natureMap[x][y]
+			if(tileID == 2):
+				print("rendering tree")
 			set_cell(Vector2i(x, y), 0, Vector2i(tileID, 0))
