1eb8ed422874c41991624e9611b8e25e51c7a313
Alex K <krab7153@gmail.com>
2025-06-27 22:56:44 +0200

    mutation operand & copying best result

--- WFC/GeneticAlgorithm.gd
+++ WFC/GeneticAlgorithm.gd
@@ -12,6 +12,8 @@ extends Object
 @export var number_of_recalculated_chunks = 0.3
 @export var initial_population_size = 4
 @export var number_of_epochs = 1
+@export var mutation_chance: float = 1.0
+
 
 var chunk_fitness_map: Dictionary = {}
 
@@ -22,6 +24,8 @@ var protected_ranges = [Vector2i(40,53), Vector2i(60, 73)]
 
 var all_generations := {} 
 # Dictionary<(epoch_number:int), Dictionary<chunk_pos:Vector2i, chunk_map:Array>>
+var best_results := {}
+
 
 func is_protected(index: int) -> bool:
 	for range in protected_ranges:
@@ -29,15 +33,6 @@ func is_protected(index: int) -> bool:
 			return true
 	return false
 	
-#func print_matrix(world_map: Array) -> void:
-	#for y in range(world_map[0].size()):
-		#var row_output := ""
-		#for x in range(world_map.size()):
-			#var val = world_map[x][y]
-			#row_output += " " + str(val.collapsedState) + " "
-		#print(row_output)
-
-
 func crossover_chunks_to_child(parent_a: Array, parent_b: Array, chunk_width: int, chunk_height: int) -> WFC:
 	var child := WFC.new(chunk_width, chunk_height)
 
@@ -60,9 +55,88 @@ func crossover_chunks_to_child(parent_a: Array, parent_b: Array, chunk_width: in
 
 	
 	child.calculateWFC()
-	#child._printEntropyMap()
 
 	return child
+
+func mutate_chunk(wfc: WFC) -> void:
+	var width = wfc.gridMatrix.size()
+	var height = wfc.gridMatrix[0].size()
+
+	var x = randi() % width
+	var y = randi() % height
+	
+	var recalculate_queue: Array = []
+	var center_tile = wfc.gridMatrix[x][y]
+
+	var possibleTilesCount = Tiles.tiles.size()
+	
+	# do not mutate if tile is restricted or has restricted neighbors:
+	if is_protected(center_tile.collapsedState):
+		return
+
+	for dir in ["top", "bottom", "left", "right"]:
+		if center_tile.neighbors.has(dir):
+			var neighbor_tile = center_tile.neighbors[dir]
+			if is_protected(neighbor_tile.collapsedState):
+				return
+	
+	
+	# reset core tile and neighbor tiles
+	center_tile.possibleStates.clear()
+	for i in range(possibleTilesCount):
+		center_tile.possibleStates.append(true)
+		
+	center_tile.collapsedState = -1
+	center_tile.entropy = center_tile.possibleStates.size()
+
+	recalculate_queue.append(center_tile)
+
+	var directions = ["top", "bottom", "left", "right"]
+	for dir in directions:
+		if wfc.gridMatrix[x][y].neighbors.has(dir):
+			var neighbor_tile = wfc.gridMatrix[x][y].neighbors[dir]
+
+			neighbor_tile.possibleStates.clear()
+			for i in range(possibleTilesCount):
+				neighbor_tile.possibleStates.append(true)
+				
+			neighbor_tile.collapsedState = -1
+			neighbor_tile.entropy = neighbor_tile.possibleStates.size()
+
+			recalculate_queue.append(neighbor_tile)
+			
+	# recalculate collapse
+	PostProcess._recalculate_collapse(recalculate_queue)
+
+
+func print_fitness_improvement_for_worst_chunks(worst_chunks: Array, world_map: Array) -> void:
+	print("\n\nFitness improvement for worst chunks")
+
+	for chunk_pos in worst_chunks:
+		if not best_results.has(chunk_pos):
+			continue
+
+		var original_chunk = world_map[chunk_pos.x][chunk_pos.y]
+		if original_chunk == null:
+			continue
+
+		var initial_fitness = evaluate_fitness(original_chunk)
+		var final_fitness = evaluate_fitness(best_results[chunk_pos])
+		var diff = final_fitness - initial_fitness
+
+		print("Chunk ", chunk_pos, ": Initial = ", str(initial_fitness),
+			", Final = ", str(final_fitness), ", Δ = ", str(diff))
+
+
+func replace_chunks_in_world_map(world_map: Array) -> void:
+	for chunk_pos in best_results.keys():
+		var winner_chunk = best_results[chunk_pos]
+		if winner_chunk == null:
+			continue
+
+		for x in range(CHUNK_WIDTH):
+			for y in range(CHUNK_HEIGHT):
+				world_map[chunk_pos.x][chunk_pos.y][x][y] = winner_chunk[x][y]
 
 
 func _init(world_map: Array, chunk_width: int, chunk_height: int) -> void:
@@ -75,8 +149,6 @@ func _init(world_map: Array, chunk_width: int, chunk_height: int) -> void:
 			var chunk = world_map[chunk_x][chunk_y]
 			if chunk == null:
 				continue
-	
-			#print_matrix(chunk)
 			
 			var fitness = evaluate_fitness(chunk)
 			
@@ -118,14 +190,37 @@ func _init(world_map: Array, chunk_width: int, chunk_height: int) -> void:
 
 				var child = crossover_chunks_to_child(parent_a["map"], parent_b["map"], CHUNK_WIDTH, CHUNK_HEIGHT)
 				
+				# mutation
+				if randf() < mutation_chance:
+					mutate_chunk(child)
+
+				
 				new_population.append({
 					"map": child.gridMatrix,
 					"fitness": evaluate_fitness(child.gridMatrix)
 				})
 
 			all_generations[epoch][chunk_pos] = new_population
+	
+	# selecting final results 
+	var final_epoch = all_generations[number_of_epochs]
 
-		
+	for chunk_pos in final_epoch.keys():
+		var population = final_epoch[chunk_pos]
+		if population.is_empty():
+			continue
+
+		population.sort_custom(func(a, b):
+			return a["fitness"] > b["fitness"]
+		)
+
+		best_results[chunk_pos] = population[0]["map"]
+	
+	# print improvment 
+	print_fitness_improvement_for_worst_chunks(worst_chunks ,world_map)
+
+	# copy winners to final map
+	replace_chunks_in_world_map(	world_map)
 
 func tournament_selection(population: Array, tournament_size: int) -> Dictionary:
 	var candidates := []
